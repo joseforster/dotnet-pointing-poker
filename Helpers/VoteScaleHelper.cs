0f06c824d6893130f4c1db2620312dbc2415a722
joseforster <jmforster2@gmail.com>
2025-11-09 14:59:49 -0300

    added constant for limit between vote scales

--- Helpers/VoteScaleHelper.cs
+++ Helpers/VoteScaleHelper.cs
@@ -1,11 +1,15 @@
 public class VoteScaleHelper
 {
-    private const string UNDECIDED_VOTE = "?";
+    public static readonly int MINIMAL_VOTE = 0;
+    public static readonly int LIMIT_BETWEEN_LOW_MEDIUM_VOTE = 12;
+    public static readonly int LIMIT_BETWEEN_MEDIUM_HIGH_VOTE = 60;
+    public static readonly string UNDECIDED_VOTE = "?";
+    
     private Dictionary<EnumVoteScale, List<int>> _voteScaleByMinMax = new()
     {
-        {EnumVoteScale.Low, new List<int>(){ 0, 12 } },
-        {EnumVoteScale.Medium, new List<int>(){ 12, 60 } },
-        {EnumVoteScale.High, new List<int>(){ 60, int.MaxValue } },
+        {EnumVoteScale.Low, new List<int>(){ MINIMAL_VOTE, LIMIT_BETWEEN_LOW_MEDIUM_VOTE } },
+        {EnumVoteScale.Medium, new List<int>(){ LIMIT_BETWEEN_LOW_MEDIUM_VOTE, LIMIT_BETWEEN_MEDIUM_HIGH_VOTE } },
+        {EnumVoteScale.High, new List<int>(){ LIMIT_BETWEEN_MEDIUM_HIGH_VOTE, int.MaxValue } },
     };
 
     public EnumVoteScale GetEnumVoteScale(string vote)
