f596c3b21cbf77d9bcf029f114e42ead4b09781b
joseforster <jmforster2@gmail.com>
2026-01-10 17:26:56 -0300

    feat: moved serilog configuration to appsettings.json, used documentation example

--- PointingPoker.Web/Program.cs
+++ PointingPoker.Web/Program.cs
@@ -1,61 +1,86 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Serilog;
+using Serilog.Templates;
+using Serilog.Templates.Themes;
 
 Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
-    .WriteTo.File("/app/logs/log-.txt", rollingInterval: RollingInterval.Day)
-    .CreateLogger();
+    .CreateBootstrapLogger();
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Information("Starting up!");
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Services.AddSerilog((services, lc) => lc
+        .ReadFrom.Configuration(builder.Configuration)
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext()
+        .WriteTo.Console(new ExpressionTemplate(
+            "[{@t:HH:mm:ss} {@l:u3}{#if @tr is not null} ({substring(@tr,0,4)}:{substring(@sp,0,4)}){#end}] {@m}\n{@x}",
+            theme: TemplateTheme.Code)));
 
 // Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddSignalR(s =>
-{
-    if (builder.Environment.IsDevelopment())
+    builder.Services.AddRazorPages();
+    builder.Services.AddSignalR(s =>
     {
-        s.EnableDetailedErrors = true;
-    }
-});
-
-builder.Services
-    .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(options =>
-    {
-        options.Cookie.Name = "PointingPokerAuth";
-        options.LoginPath = "/Login";
-        options.ExpireTimeSpan = TimeSpan.FromDays(3);
+        if (builder.Environment.IsDevelopment())
+        {
+            s.EnableDetailedErrors = true;
+        }
     });
 
-builder.Services.AddSerilog();
+    builder.Services
+        .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+        .AddCookie(options =>
+        {
+            options.Cookie.Name = "PointingPokerAuth";
+            options.LoginPath = "/Login";
+            options.ExpireTimeSpan = TimeSpan.FromDays(3);
+        });
 
-var app = builder.Build();
+    var app = builder.Build();
 
-var cookiePolicyOptions = new CookiePolicyOptions
-{
-    MinimumSameSitePolicy = SameSiteMode.Strict,
-};
+    var cookiePolicyOptions = new CookiePolicyOptions
+    {
+        MinimumSameSitePolicy = SameSiteMode.Strict,
+    };
 
-app.UseCookiePolicy(cookiePolicyOptions);
+    app.UseCookiePolicy(cookiePolicyOptions);
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseSerilogRequestLogging();
+
+    app.UseRouting();
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.MapStaticAssets();
+    app.MapRazorPages().WithStaticAssets();
+
+    app.MapHub<PointingPokerHub>("/pointingpokerhub");
+
+    await app.RunAsync();
+    
+    Log.Information("Stopped cleanly");
+}
+catch (Exception ex)
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    Log.Fatal(ex, "An unhandled exception occurred during bootstrapping");
+}
+finally
+{
+    Log.CloseAndFlush();
 }
 
-app.UseHttpsRedirection();
-
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapStaticAssets();
-app.MapRazorPages().WithStaticAssets();
-
-app.MapHub<PointingPokerHub>("/pointingpokerhub");
-
-app.Run();
